ff89a015758f595aa782d20e7808e3f59fd4fc84
priyankpuri <priyankpuri@hotmail.com>
2015-05-07 14:19:36 +0530

    Adding the plot2 image as previous was corrupted

--- plot2.R
+++ plot2.R
@@ -12,5 +12,5 @@ sub_data$Datetime <- as.POSIXct(datetime)
 
 ## Making the plot
 plot(sub_data$Global_active_power~sub_data$Datetime, type="l",ylab="Global Active Power (kilowatts)", xlab="")
-dev.copy(png, file="plot3.png", height=480, width=480)
+dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
